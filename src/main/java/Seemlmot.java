2e35620d020cf5daf80b10dd580fc22616a1fd8e
seeml-mo <panhuangyu088@gmail.com>
2026-01-29 23:33:21 +0800

    Level 1 Echo & Level 2 Add. List are finished

--- src/main/java/Seemlmot.java
+++ src/main/java/Seemlmot.java
@@ -1,4 +1,6 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Seemlmot {
     public static void main(String[] args) {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -9,17 +11,39 @@ public class Duke {
 
         String Greeting = """
                 ____________________________________________________________
-                Hello! I'm Seeml-mot
+                Hello! I'm Seemlmot
                 What can I do for you?
                 ____________________________________________________________
                 """;
         System.out.println(Greeting);
 
         String Exit = """
+                ____________________________________________________________
                 Bye. Hope to see you again soon!
                 ____________________________________________________________
                 """;
 
+        String horizontalLine = "____________________________________________________________";
+
+        Scanner in = new Scanner(System.in);
+        String[] cmdList = new String[100];
+        String currentCmd = in.nextLine();
+
+        int countCmd = 0;
+        while(!currentCmd.equals("bye") && countCmd < 100) {
+                System.out.println(horizontalLine);
+                if( currentCmd.equals("list") ){
+                    for(int i = 0; i < countCmd; i++)
+                        System.out.println( i+1 + ". " + cmdList[i]);
+                }
+                else {
+                    cmdList[countCmd++] = currentCmd;
+                    System.out.println("added: " + currentCmd);
+                }
+
+                System.out.println(horizontalLine);
+                currentCmd = in.nextLine();
+        }
         System.out.println(Exit);
     }
 }
