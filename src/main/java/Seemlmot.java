e9a5e7e69799af3582c1cef72a53a89611018509
seeml-mo <panhuangyu088@gmail.com>
2026-01-29 11:54:55 +0800

    Rename the bot, and the Greeting is set

---
+++ src/main/java/Seemlmot.java
@@ -0,0 +1,25 @@
+public class Duke {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);*/
+
+        String Greeting = """
+                ____________________________________________________________
+                Hello! I'm Seeml-mot
+                What can I do for you?
+                ____________________________________________________________
+                """;
+        System.out.println(Greeting);
+
+        String Exit = """
+                Bye. Hope to see you again soon!
+                ____________________________________________________________
+                """;
+
+        System.out.println(Exit);
+    }
+}
