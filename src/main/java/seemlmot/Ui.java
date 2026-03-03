8afe071aa13ddb6c29dd0f2f1265f27fb0b3ef9b
seeml-mo <panhuangyu088@gmail.com>
2026-03-03 10:03:04 +0800

    Logo added

--- src/main/java/seemlmot/Ui.java
+++ src/main/java/seemlmot/Ui.java
@@ -8,10 +8,17 @@ package seemlmot;
 public class Ui {
     public static final String HORIZONTAL_LINE = "____________________________________________________________";
     private static final String GREETING = """
-                 Hello! I'm Seemlmot
                  What can I do for you?
                 ____________________________________________________________
                 """;
+    private static final String LOGO =
+            """
+                     ____                       _                  _  \s
+                    / ___|  ___  ___ _ __ ___  | | _ __ ___   ___ | |_\s
+                    \\___ \\ / _ \\/ _ \\ '_ ` _ \\ | || '_ ` _ \\ / _ \\| __|
+                     ___) |  __/  __/ | | | | || || | | | | | (_) | |_\s
+                    |____/ \\___|\\___|_| |_| |_||_||_| |_| |_|\\___/ \\__|
+                    """;
     private static final String EXIT = """
             ____________________________________________________________
             Bye. Hope to see you again soon!
@@ -28,7 +35,8 @@ public class Ui {
 
         TaskList.initialize( Storage.loadData() );
 
-        System.out.println(GREETING);
+        System.out.println("Hello! I'm Seemlmot");
+        System.out.println(LOGO + GREETING);
     }
 
     /**
